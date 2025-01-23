c225703b8221d93f4e5a35fd4fb0217123b57e02
vamsi-web <102210130+vamsi-web@users.noreply.github.com>
2025-01-23 22:01:02 +0530

    Update diffusion.py

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -316,8 +316,9 @@ class Diffusion(object):
                 idx = torch.cat([idx_1, idx_2], dim=0)[:n]
                 t = t_intervals[idx].to(self.device)
 
-                loss = loss_registry[config.model.type](model, x_img, x_img, x_gt, t, e, b)
-
+                #loss = loss_registry[config.model.type](model, x_img, x_img x_gt, t, e, b)
+                loss = loss_registry[config.model.type](model, x_gt, t, e, b)
+                
                 tb_logger.add_scalar("loss", loss, global_step=step)
 
                 logging.info(f"step: {step}, loss: {loss.item()}")
