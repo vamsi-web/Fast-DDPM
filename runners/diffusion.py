b3ecb58b98a3fb61d79d0338c617fab53328dda8
vamsi-web <102210130+vamsi-web@users.noreply.github.com>
2025-02-04 19:37:42 +0530

    Update diffusion.py

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -471,6 +471,104 @@ class Diffusion(object):
                     )
                     torch.save(states, os.path.join(self.args.log_path, "ckpt.pth"))
 
+
+    def pet_ddpm_train(self):
+        args, config = self.args, self.config
+        tb_logger = self.config.tb_logger
+
+        dataset = PMUB(self.config.data.train_dataroot, self.config.data.image_size, split='train')
+        print('Start PET training DDPM model on PMUB dataset.')
+        print('The number of involved time steps is {} out of 1000.'.format(self.args.timesteps))
+
+        train_loader = data.DataLoader(
+            dataset,
+            batch_size=config.training.batch_size,
+            shuffle=True,
+            num_workers=config.data.num_workers,
+            pin_memory=True)
+
+        model = Model(config)
+        model = model.to(self.device)
+        model = torch.nn.DataParallel(model)
+
+        optimizer = get_optimizer(self.config, model.parameters())
+
+        if self.config.model.ema:
+            ema_helper = EMAHelper(mu=self.config.model.ema_rate)
+            ema_helper.register(model)
+        else:
+            ema_helper = None
+
+        start_epoch, step = 0, 0
+        if self.args.resume_training:
+            states = torch.load(os.path.join(self.args.log_path, "ckpt.pth"))
+            model.load_state_dict(states[0])
+
+            states[1]["param_groups"][0]["eps"] = self.config.optim.eps
+            optimizer.load_state_dict(states[1])
+            start_epoch = states[2]
+            step = states[3]
+            if self.config.model.ema:
+                ema_helper.load_state_dict(states[4])
+
+        time_start = time.time()
+        total_time = 0
+        for epoch in range(start_epoch, self.config.training.n_epochs):
+            for i, x in enumerate(train_loader):
+                n = x['BW'].size(0)
+                model.train()
+                step += 1
+
+                x_bw = x['BW'].to(self.device)
+                x_md = x['MD'].to(self.device)
+                x_fw = x['FW'].to(self.device)
+                x_pet = x['PET'].to(self.device)  # PET-specific input
+
+                e = torch.randn_like(x_md)
+                b = self.betas
+
+                # Antithetic sampling
+                t = torch.randint(
+                    low=0, high=self.num_timesteps, size=(n // 2 + 1,)
+                ).to(self.device)
+                t = torch.cat([t, self.num_timesteps - t - 1], dim=0)[:n]
+            
+                # PET-specific loss
+                #loss = loss_registry[config.model.type](model, x_bw, x_md, x_fw, x_pet, t, e, b)
+            
+                tb_logger.add_scalar("loss", loss, global_step=step)
+
+                logging.info(f"step: {step}, loss: {loss.item()}")
+
+                optimizer.zero_grad()
+                loss.backward()
+            
+                try:
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), config.optim.grad_clip)
+                except Exception:
+                    pass
+
+                optimizer.step()
+
+                if self.config.model.ema:
+                    ema_helper.update(model)
+
+                if step % self.config.training.snapshot_freq == 0 or step == 1:
+                    states = [
+                        model.state_dict(),
+                        optimizer.state_dict(),
+                        epoch,
+                        step,
+                    ]
+                    if self.config.model.ema:
+                        states.append(ema_helper.state_dict())
+
+                    torch.save(
+                        states,
+                        os.path.join(self.args.log_path, "ckpt_{}.pth".format(step)),
+                    )
+                    torch.save(states, os.path.join(self.args.log_path, "ckpt.pth"))
+
     
     # Training original DDPM for tasks that have only one condition: image translation and CT denoising.
     def sg_ddpm_train(self):
