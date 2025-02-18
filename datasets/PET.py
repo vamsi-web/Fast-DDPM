0b83f949b2741a82972af90ac035642e2969952c
vamsi-web <102210130+vamsi-web@users.noreply.github.com>
2025-02-18 19:40:21 +0530

    Update PET.py

--- datasets/PET.py
+++ datasets/PET.py
@@ -41,7 +41,7 @@ class PETDataset(Dataset):
         h, w, c = img.shape
         #if w == 256 and c == 1 and h != 128:
             #img = np.resize(img, (128, 256, 1))  # Resize to correct height (128)
-        img = img.squeeze(2)
+        #img = img.squeeze(2)
         # Split the image into LPET and FDPET
         lpet = img[:128, :, :]  # Left half (LPET)
         fdpet = img[128:, :, :]  # Right half (FDPET)
